686c736aec25d9e62bc618d817f93dc2f3594c54
Dmitry <profydima@yandex.ru>
2022-12-27 09:49:12 +0300

    решил вторую задачу

--- Program.cs
+++ Program.cs
@@ -13,3 +13,21 @@ else
         System.Console.Write($"Число {num3} - Не полиндром");
 }
 
+// Задача 2
+
+System.Console.WriteLine("Введите координату X точки A: ");
+int x1 = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Введите координату Y точки A: ");
+int y1 = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Введите координату Z точки A: ");
+int z1 = Convert.ToInt32(Console.ReadLine());
+
+System.Console.WriteLine("Введите координату X точки B: ");
+int x2 = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Введите координату Y точки B: ");
+int y2 = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Введите координату Z точки B: ");
+int z2 = Convert.ToInt32(Console.ReadLine());
+
+double result = Math.Round(Math.Sqrt(Math.Pow((x1 - x2),2) + Math.Pow((y1 - y2),2) + Math.Pow((z1 - z2),2)),2); 
+System.Console.WriteLine($"Расстояние между тремя точками: {result}");
