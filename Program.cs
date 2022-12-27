27e1538426201c834181186e7d0f8a3a33ee8f64
Dmitry <profydima@yandex.ru>
2022-12-27 09:40:13 +0300

    решил перую задачу

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 1
+
+System.Console.WriteLine("Введите пятизначное число: ");
+int num = Convert.ToInt32(System.Console.ReadLine());
+
+string num3 = Convert.ToString(num);
+if (num3[0] == num3[4] && num3[1] == num3[3])
+{
+        System.Console.Write($"Число {num3} - Полиндром!");
+}
+else
+{
+        System.Console.Write($"Число {num3} - Не полиндром");
+}
+
